de16592d2139d054f3d2c4870478e5c6f3588cae
DiyorMr <dmuxiddinov594@gmail.com>
2025-05-16 19:44:35 +0500

    fixed a bugs

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,16 +13,16 @@ const Header = () => {
                             <li className="header_container_list_item">
                                 <a href="#features">Features</a>
                             </li>
-                            <li className="header_container_list_item_link">
+                            <li className="header_container_list_item">
                                 <a href="#works">How it works</a>
                             </li>
-                            <li className="header_container_list_item_link">
+                            <li className="header_container_list_item">
                                 <a href="#support">Use Cases</a>
                             </li>
-                            <li className="header_container_list_item_link">
+                            <li className="header_container_list_item">
                                 <a href="#pricing">Pricing</a>
                             </li>
-                            <li className="header_container_list_item_link">
+                            <li className="header_container_list_item">
                                 <a href="#faq">Faq</a>
                             </li>
                         </ul>
